e4ee1a35b7b25f09f4ee759e03d317aa64506002
ArelaVka <arelav90@mail.ru>
2019-05-07 21:29:59 +0300

    add salary
    need to do  def predict_rub_salary

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ url = 'https://api.hh.ru/vacancies'
 
 vacancies_count = {}
 
-language_list = ['python', 'java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby']
+#language_list = ['python', 'java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby']
+language_list = ['python']
 
 for language in language_list:
   headers = {
@@ -16,6 +17,20 @@ for language in language_list:
   }
   response = requests.get(url, headers=headers, params=params)
   #print(response.url)
-  count = response.json()
-  vacancies_count.update({language : count['found']})
-print(vacancies_count)
+  json_response = response.json()
+  vacancies_count.update({language : json_response['found']})
+  for item in json_response['items']:
+    #print(item['salary'])
+    
+    if (item['salary'] is not None) and (item['salary']['currency'] == 'RUR'):
+      if (item['salary']['from'] is not None) and (item['salary']['to'] is not None):
+        print((item['salary']['from']+item['salary']['to'])/2)
+      elif (item['salary']['from'] is not None):
+        print(item['salary']['from'] * 2)
+      elif (item['salary']['to'] is not None):
+        print(item['salary']['to'] * 0.8)
+    else:
+      print('None')
+    
+      
+#print(vacancies_count)
