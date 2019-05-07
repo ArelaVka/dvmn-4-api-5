82cf170fa82bd0934af0f4839ac527cb992e13a9
ArelaVka <arelav90@mail.ru>
2019-05-07 20:53:34 +0300

    get count of vacancies

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import requests
 
 url = 'https://api.hh.ru/vacancies'
-headers = {
+
+vacancies_count = {}
+
+language_list = ['python', 'java', 'Javascript']
+
+for language in language_list:
+  headers = {
     'User-Agent': 'HH-User-Agent'
-}
-response = requests.get(url, headers=headers)
-print(response.json())
+    }
+  params = {
+    'area': 1,
+    'text': 'программист' + language
+  }
+  response = requests.get(url, headers=headers, params=params)
+  print(response.url)
+  count = response.json()
+  vacancies_count.update({language : count['found']})
+print(vacancies_count)
