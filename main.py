2d672737f2197cf1bc270a57b4b1f11c964671d7
ArelaVka <arelav90@mail.ru>
2019-07-01 15:28:11 +0300

    hh complete

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import requests
+import json
+
+
+def predict_rub_salary(vacancy):
+  if (vacancy['salary'] is not None) and (vacancy['salary']['currency'] == 'RUR'):
+      if (vacancy['salary']['from'] is not None) and (vacancy['salary']['to'] is not None):
+        return int((vacancy['salary']['from']+vacancy['salary']['to'])/2)
+      elif (vacancy['salary']['from'] is not None):
+        return int(vacancy['salary']['from'] * 2)
+      elif (vacancy['salary']['to'] is not None):
+        return int(vacancy['salary']['to'] * 0.8)
+  else:
+    return 'None'
+
 
 url = 'https://api.hh.ru/vacancies'
+middle_language_price = {}
 
-vacancies_count = {}
-
-#language_list = ['python', 'java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby']
-language_list = ['python']
+language_list = ['python', 'java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby']
 
 for language in language_list:
   headers = {
@@ -16,21 +28,16 @@ for language in language_list:
     'text': 'программист' + language
   }
   response = requests.get(url, headers=headers, params=params)
-  #print(response.url)
   json_response = response.json()
-  vacancies_count.update({language : json_response['found']})
-  for item in json_response['items']:
-    #print(item['salary'])
-    
-    if (item['salary'] is not None) and (item['salary']['currency'] == 'RUR'):
-      if (item['salary']['from'] is not None) and (item['salary']['to'] is not None):
-        print((item['salary']['from']+item['salary']['to'])/2)
-      elif (item['salary']['from'] is not None):
-        print(item['salary']['from'] * 2)
-      elif (item['salary']['to'] is not None):
-        print(item['salary']['to'] * 0.8)
-    else:
-      print('None')
-    
-      
-#print(vacancies_count)
+
+  
+  vacancies_processed = 0
+  average_salary = 0
+
+  for vacancy in json_response['items']:
+    if predict_rub_salary(vacancy) is not 'None':
+      vacancies_processed += 1
+      average_salary = int((average_salary + predict_rub_salary(vacancy)) / 2
+  middle_language_price[language] = {'vacancies_found': json_response['found'], 'vacancies_processed': vacancies_processed, 'average_salary': average_salary}
+
+print(json.dumps(middle_language_price, indent=4, sort_keys=True))
