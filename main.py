e09da460879b0a062b4de7867081af05a5aa2da1
ArelaVka <arelav90@mail.ru>
2019-07-01 18:50:45 +0300

    hh final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from itertools import count
 
 
 def predict_rub_salary(vacancy):
@@ -17,27 +18,35 @@ def predict_rub_salary(vacancy):
 url = 'https://api.hh.ru/vacancies'
 middle_language_price = {}
 
-language_list = ['python', 'java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby']
+language_list = ['Python', 'Java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby']
+#language_list = ['Python']
 
 for language in language_list:
+  page = 0
+  pages_number = 1
+
   headers = {
     'User-Agent': 'HH-User-Agent'
-    }
+  }
   params = {
     'area': 1,
-    'text': 'программист' + language
+    'text': 'программист' + language,
+    'page': page
   }
-  response = requests.get(url, headers=headers, params=params)
-  json_response = response.json()
 
-  
   vacancies_processed = 0
   average_salary = 0
+  summary_salary = 0
+  while page < pages_number:
+    page_data = requests.get(url, headers=headers, params=params).json()
+    pages_number = page_data['pages']
+    page += 1
+    print('PAGE {}'.format(page))
+    for vacancy in page_data['items']:
+      if predict_rub_salary(vacancy) is not 'None':
+        vacancies_processed += 1
+        summary_salary = int(summary_salary + predict_rub_salary(vacancy))
+    average_salary = int(summary_salary / vacancies_processed)
+  middle_language_price[language] = {'vacancies_found': page_data['found'], 'vacancies_processed': vacancies_processed, 'average_salary': average_salary}
 
-  for vacancy in json_response['items']:
-    if predict_rub_salary(vacancy) is not 'None':
-      vacancies_processed += 1
-      average_salary = int((average_salary + predict_rub_salary(vacancy)) / 2
-  middle_language_price[language] = {'vacancies_found': json_response['found'], 'vacancies_processed': vacancies_processed, 'average_salary': average_salary}
-
-print(json.dumps(middle_language_price, indent=4, sort_keys=True))
+  print(json.dumps(middle_language_price, indent=4, sort_keys=True))
