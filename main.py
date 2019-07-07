c024ad225c0a0b1f8957e1280fb5f4397e2205fb
ArelaVka <arelav90@mail.ru>
2019-07-07 16:00:44 +0300

    add sj-block and pretty output
    add sj-block and pretty output with terminaltables

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+from terminaltables import AsciiTable
+
 
 load_dotenv()
 
@@ -37,13 +39,13 @@ def get_predict_rub_salary_sj(vacancy):
   else:
     return None
 
-'''
+language_list = ['Python', 'Java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby', 'c++', 'c#', '1c']
+#language_list = ['c#']
+
+
 #---------------------hh.ru:---------------------
 hh_url = 'https://api.hh.ru/vacancies'
-middle_language_price = {}
-
-language_list = ['Python', 'Java', 'Javascript','TypeScript', 'Swift', 'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby']
-#language_list = ['Python']
+middle_language_price_hh = {}
 
 for language in language_list:
   page = 0
@@ -69,50 +71,85 @@ for language in language_list:
     for vacancy in page_data['items']:
       if get_predict_rub_salary_hh(vacancy) is not None:
         vacancies_processed += 1
-        print('vacancies_processed = {}, salary = {}, sum = {}'.format(vacancies_processed, get_predict_rub_salary_hh(vacancy), summary_salary))
+        #print('vacancies_processed = {}, salary = {}, sum = {}'.format(vacancies_processed, get_predict_rub_salary_hh(vacancy), summary_salary))
         summary_salary = int(summary_salary + get_predict_rub_salary_hh(vacancy))
         average_salary = int(summary_salary / vacancies_processed)
-  middle_language_price[language] = {
+  middle_language_price_hh[language] = {
     'vacancies_found': page_data['found'],
     'vacancies_processed': vacancies_processed,
     'average_salary': average_salary
     }
 
-print(json.dumps(middle_language_price, indent=4, sort_keys=True))
-'''
+print(json.dumps(middle_language_price_hh, indent=4, sort_keys=True))
+
+title = '--------------------HH statistics'
+TABLE_DATA = [['lang', 'vacancies_found', 'vacancies_processed', 'average_salary']]
+
+for main_key, main_value in middle_language_price_hh.items():
+  row=[]
+  row.append(main_key)
+  for key, value in main_value.items():
+    row.append(value)
+  print(row)
+  TABLE_DATA.append(row)
+
+table_instance = AsciiTable(TABLE_DATA, title)
+print(table_instance.table)
+
 
 #--------------------superjob:---------------------
 secret_key = os.getenv("SJ_KEY")
 sj_url='https://api.superjob.ru/2.0/vacancies'
-headers = {
-  'X-Api-App-Id': secret_key
-}
-params = {
-  'page': 5, #Номер страницы результата поиска
-  'count': 100, #Количество результатов на страницу поиска
-  'town': 4, #Название города или его ID. 4 - Москва
-  'catalogues': 48, #Список разделов каталога отраслей. Список значений можно узнать из метода catalogues, параметр key в любом объекте
-  'no_agreement': 1 #не показывать оклад по договоренности
-}
 
-#response = requests.get(sj_url, headers=headers, params=params)
-#print(response.url)
-#print(response.json())
-#vacancy['profession'] == 'Программист'
-#vacancy['town']['id'] == 4
-#vacancy['catalogues'][0]['positions'][0]['key'] == 48
+middle_language_price_sj = {}
 
-page_data = requests.get(sj_url, headers=headers, params=params).json()
-print(page_data['more'])
-i=0
-for vacancy in page_data['objects']:
-  i+=1
-  print('{}) {}, {}: {}-{} ({}) {}'.format(i, vacancy['profession'], vacancy['town']['title'], vacancy['payment_from'], vacancy['payment_to'], get_predict_rub_salary_sj(vacancy), vacancy['currency']))
-'''
-  for vacancy in page_data['objects']:
-    #print (vacancy['catalogues'][0]['id']);
-    if vacancy['catalogues'][0]['id'] == 438:
-      print('{}, {} - {}'.format(vacancy['profession'], vacancy['town']['title'], vacancy['catalogues'][0]['positions'][0]['key']))
-#print(page_data['objects'])
-#print(page_data.text)
-'''
+
+for language in language_list:
+  page = 0
+  vacancies_processed = 0
+  average_salary = 0
+  summary_salary = 0  
+  next_page = True
+  while (next_page):
+    headers = {
+    'X-Api-App-Id': secret_key
+    }
+    params = {
+    'page': page, #Номер страницы результата поиска
+    'count': 5, #Количество результатов на страницу поиска
+    'keyword': language,
+    'town': 4, #Название города или его ID. 4 - Москва
+    'catalogues': 48, #Список разделов каталога отраслей. Список значений можно узнать из метода catalogues, параметр key в любом объекте. 48 - "Разработка, программирование"
+    'no_agreement': 1 #Без вакансий, где оклад по договоренности
+    }
+    page_data = requests.get(sj_url, headers=headers, params=params).json()
+    next_page = page_data['more']
+    page = page + 1
+    print('Download {}, page {}'.format(language, page))
+    for vacancy in page_data['objects']:
+      vacancies_processed += 1
+      #print('{}, {}: {}-{} ({}) {}'.format(vacancy['profession'], vacancy['town']['title'], vacancy['payment_from'], vacancy['payment_to'], get_predict_rub_salary_sj(vacancy), vacancy['currency']))
+      summary_salary = int(summary_salary + get_predict_rub_salary_sj(vacancy))
+      average_salary = int(summary_salary / vacancies_processed)
+
+  middle_language_price_sj[language] = {
+    'vacancies_found': page_data['total'],
+    'vacancies_processed': vacancies_processed,
+    'average_salary': average_salary
+    }
+
+#print(json.dumps(middle_language_price_sj, indent=4, sort_keys=True))
+
+title = '--------------------SJ statistics'
+TABLE_DATA = [['lang', 'vacancies_found', 'vacancies_processed', 'average_salary']]
+
+for main_key, main_value in middle_language_price_sj.items():
+  row=[]
+  row.append(main_key)
+  for key, value in main_value.items():
+    row.append(value)
+  print(row)
+  TABLE_DATA.append(row)
+
+table_instance = AsciiTable(TABLE_DATA, title)
+print(table_instance.table)
