e98f8ac478f8aeeb110926756f3d02ce146d7be8
ArelaVka <arelav90@mail.ru>
2019-07-17 17:01:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,15 @@ def get_predict_rub_salary_sj(vacancy):
         return None
 
 
+def check_request(request):
+    try:
+        request.raise_for_status()
+        return request
+    except requests.exceptions.HTTPError as error:
+        print('ERROR: {}'.format(error))
+        return None
+
+
 def get_hh_statistic_dict(language_list):
     hh_url = 'https://api.hh.ru/vacancies'
     middle_language_price_hh = {}
@@ -49,19 +58,22 @@ def get_hh_statistic_dict(language_list):
         vacancies_processed = 0
         summary_salary = 0
         while page < pages_number:
-            page_data = requests.get(hh_url, headers=headers, params=params).json()
-            pages_number = page_data['pages']
+            page_data = requests.get(hh_url, headers=headers, params=params)
             page += 1
-            for vacancy in page_data['items']:
-                if get_predict_rub_salary_hh(vacancy) is not None:
-                    vacancies_processed += 1
-                    summary_salary = int(summary_salary + get_predict_rub_salary_hh(vacancy))
-                    average_salary = int(summary_salary / vacancies_processed)
-                    middle_language_price_hh[language] = {
-                        'vacancies_found': page_data['found'],
-                        'vacancies_processed': vacancies_processed,
-                        'average_salary': average_salary
-                    }
+            if check_request(page_data):
+                page_json_data = page_data.json()
+                pages_number = page_json_data['pages']
+
+                for vacancy in page_json_data['items']:
+                    if get_predict_rub_salary_hh(vacancy) is not None:
+                        vacancies_processed += 1
+                        summary_salary = int(summary_salary + get_predict_rub_salary_hh(vacancy))
+                        average_salary = int(summary_salary / vacancies_processed)
+                        middle_language_price_hh[language] = {
+                            'vacancies_found': page_json_data['found'],
+                            'vacancies_processed': vacancies_processed,
+                            'average_salary': average_salary
+                        }
     return middle_language_price_hh
 
 
@@ -86,38 +98,42 @@ def get_sj_statistic_dict(language_list):
                 'catalogues': 48,  # Список разделов каталога отраслей. 48 - "Разработка, программирование"
                 'no_agreement': 1  # Без вакансий, где оклад по договоренности
             }
-            page_data = requests.get(sj_url, headers=headers, params=params).json()
-            next_page = page_data['more']
-            page = page + 1
-            for vacancy in page_data['objects']:
-                vacancies_processed += 1
-                summary_salary = int(summary_salary + get_predict_rub_salary_sj(vacancy))
-                average_salary = int(summary_salary / vacancies_processed)
-                middle_language_price_sj[language] = {
-                    'vacancies_found': page_data['total'],
-                    'vacancies_processed': vacancies_processed,
-                    'average_salary': average_salary
-                }
+            page_data = requests.get(sj_url, headers=headers, params=params)
+            if check_request(page_data):
+                page_json_data = page_data.json()
+                next_page = page_json_data['more']
+                page = page + 1
+                for vacancy in page_json_data['objects']:
+                    vacancies_processed += 1
+                    summary_salary = int(summary_salary + get_predict_rub_salary_sj(vacancy))
+                    average_salary = int(summary_salary / vacancies_processed)
+                    middle_language_price_sj[language] = {
+                        'vacancies_found': page_json_data['total'],
+                        'vacancies_processed': vacancies_processed,
+                        'average_salary': average_salary
+                    }
+            else:
+                next_page = False
     return middle_language_price_sj
 
 
 def make_table(title, statistic_dict):
-    table_data = [['lang', 'vacancies_found', 'vacancies_processed', 'average_salary']]
-    for language, language_stat in statistic_dict.items():
-        row = []
-        row.append(language)
-        for key, value in language_stat.items():
-            row.append(value)
-        table_data.append(row)
-    table_instance = AsciiTable(table_data, title)
-    return table_instance.table
+    if statistic_dict:
+        table_data = [['lang', 'vacancies_found', 'vacancies_processed', 'average_salary']]
+        for language, language_stat in statistic_dict.items():
+            row = [language]
+            for key, value in language_stat.items():
+                row.append(value)
+            table_data.append(row)
+        return AsciiTable(table_data, title).table
+    else:
+        return 'No data was found'
 
 
 if __name__ == "__main__":
     load_dotenv()
-    #language_list = ['Python', 'Java', 'Javascript', 'TypeScript', 'Swift',
-    #               'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby', 'c++', 'c#', '1c']
-    language_list = ['Javascript', 'TypeScript', 'Swift']
+    language_list = ['Python', 'Java', 'Javascript', 'TypeScript', 'Swift',
+                   'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby', 'c++', 'c#', '1c']
     title = '--------------------HH statistics'
     print(make_table(title, get_hh_statistic_dict(language_list)))
 
