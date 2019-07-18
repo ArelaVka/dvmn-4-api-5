06d58d69327a834aff08dc65560388be7772edec
ArelaVka <arelav90@mail.ru>
2019-07-18 18:57:56 +0300

    simplify get_hh_statistic func

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_predict_salary(salary_from, salary_to):
 
 
 def get_predict_rub_salary_hh(vacancy):
-    if (vacancy['salary'] is not None) and (vacancy['salary']['currency'] == 'RUR'):
+    if (vacancy['salary']) and (vacancy['salary']['currency'] == 'RUR'):
         salary_from = (vacancy['salary']['from']) or 0
         salary_to = (vacancy['salary']['to']) or 0
         return get_predict_salary(salary_from, salary_to)
@@ -41,39 +41,40 @@ def check_request(request):
         return None
 
 
-def get_hh_statistic_dict(language_list):
+def get_hh_statistic(language_list):
     hh_url = 'https://api.hh.ru/vacancies'
     middle_language_price_hh = {}
     for language in language_list:
+        page_data = []
         page = 0
         pages_number = 1
-        headers = {
-            'User-Agent': 'HH-User-Agent'
-        }
-        params = {
-            'area': 1,
-            'text': 'программист' + language,
-            'page': page
-        }
         vacancies_processed = 0
         summary_salary = 0
         while page < pages_number:
-            page_data = requests.get(hh_url, headers=headers, params=params)
+            headers = {
+                'User-Agent': 'HH-User-Agent'
+            }
+            params = {
+                'area': 1,
+                'text': 'программист ' + language,
+                'page': page
+            }
+            page_request = requests.get(hh_url, headers=headers, params=params)
             page += 1
-            if check_request(page_data):
-                page_json_data = page_data.json()
-                pages_number = page_json_data['pages']
-
-                for vacancy in page_json_data['items']:
-                    if get_predict_rub_salary_hh(vacancy) is not None:
-                        vacancies_processed += 1
-                        summary_salary = int(summary_salary + get_predict_rub_salary_hh(vacancy))
-                        average_salary = int(summary_salary / vacancies_processed)
-                        middle_language_price_hh[language] = {
-                            'vacancies_found': page_json_data['found'],
-                            'vacancies_processed': vacancies_processed,
-                            'average_salary': average_salary
-                        }
+            if check_request(page_request):
+                page_data.append(page_request.json()['items'])
+                pages_number = page_request.json()['pages']
+        for vacancies in page_data:
+            for vacancy in vacancies:
+                if get_predict_rub_salary_hh(vacancy):
+                    vacancies_processed += 1
+                    summary_salary = int(summary_salary + get_predict_rub_salary_hh(vacancy))
+                    average_salary = int(summary_salary / vacancies_processed)
+                    middle_language_price_hh[language] = {
+                        'vacancies_found': page_request.json()['found'],
+                        'vacancies_processed': vacancies_processed,
+                        'average_salary': average_salary
+                    }
     return middle_language_price_hh
 
 
@@ -134,7 +135,7 @@ def make_table(site_name, statistic_dict):
 if __name__ == "__main__":
     load_dotenv()
     language_list = ['Python', 'Java', 'Javascript', 'TypeScript', 'Swift',
-                   'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby', 'c++', 'c#', '1c']
+                     'Scala', 'Objective-C', 'Shell', 'Go', 'C', 'PHP', 'Ruby', 'c++', 'c#', '1c']
     site_name = 'HH'
     print(make_table(site_name, get_hh_statistic(language_list)))
 
